ac45e66771ca217634a2a0470c0de87f495b63e9
Daniel Ndayegamiye <d.ndayegamiye@eagles.oc.edu>
2025-02-03 09:29:41 -0600

    added backend handling and home page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import Login from "../views/Login.vue";
+import Home from "../views/Home.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,8 +11,15 @@ const router = createRouter({
       alias: "/login",
       name: "login",
       component: Login,
-    }
+    },
+    {
+      path: "/home",
+      name: "home",
+      component: Home,
+    },
+
   ],
 });
+
 
 export default router;
