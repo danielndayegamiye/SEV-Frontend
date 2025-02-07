a4f9c975c12f8cd7778ced421234e0a8c586a9f0
Israel <israrelo@gmail.com>
2025-02-07 13:49:32 -0600

    Fixes

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/index.js";
 import vuetify from "./plugins/vuetify.js";
+import './assets/global.css';
 
 createApp(App).use(vuetify).use(router).mount("#app");
